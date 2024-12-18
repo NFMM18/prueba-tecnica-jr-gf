c4abe2bb00956513c59b3ff30802cc193755ad51
NFMM18 <nickolasmorales18@gmail.com>
2024-12-18 00:40:36 -0300

    inicio de proyecto angular

---
+++ prueba-tecnica-jr/src/app/modules/protected-page/page-principal/page-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-principal',
+  imports: [],
+  templateUrl: './page-principal.component.html',
+  styleUrl: './page-principal.component.css'
+})
+export class PagePrincipalComponent {
+
+}
